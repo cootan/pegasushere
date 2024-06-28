fde9f83e35c31753f4e8feecde2efefd3f229602
cootan <cotooanshort@gmail.com>
2024-06-28 09:55:41 +0530

    bfb

--- api/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Setting up Python environment..."
-python -m compileall .
-
-echo "Build completed successfully!"
